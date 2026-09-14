01baae92f8e11a3daa4a467ed77fa994ead85cbb
rbxrootx <rbxrootx@gmail.com>
2026-09-14 12:18:07 -0400

    Match semantic and return-call wrappers

---
+++ src/game-server/matches/RecoveredSemanticWrappers.cpp
@@ -0,0 +1,693 @@
+class Recovered00403F80
+{
+    char padding[12];
+    int first;
+    int second;
+public:
+    int Different();
+};
+
+int Recovered00403F80::Different()
+{
+    return first != second;
+}
+
+class Recovered0041A790
+{
+    int value;
+public:
+    int Equal(const Recovered0041A790 &input);
+};
+
+int Recovered0041A790::Equal(const Recovered0041A790 &input)
+{
+    return value == input.value;
+}
+
+class Recovered0041AEA0
+{
+public:
+    int Run(int input);
+};
+
+int Recovered0041AEA0::Run(int input)
+{
+    return input;
+}
+
+void Recovered0041CB20(int *first, int *second)
+{
+    int temporary = *first;
+    *first = *second;
+    *second = temporary;
+}
+
+class Recovered0041EDD0
+{
+    int value;
+public:
+    int Equal(const Recovered0041EDD0 &input);
+};
+
+int Recovered0041EDD0::Equal(const Recovered0041EDD0 &input)
+{
+    return value == input.value;
+}
+
+class Recovered0041F540
+{
+public:
+    int Run(int input);
+};
+
+int Recovered0041F540::Run(int input)
+{
+    return input;
+}
+
+void Recovered004211C0(int *first, int *second)
+{
+    int temporary = *first;
+    *first = *second;
+    *second = temporary;
+}
+
+class Recovered00440670
+{
+    int value;
+public:
+    int Equal(const Recovered00440670 &input);
+};
+
+int Recovered00440670::Equal(const Recovered00440670 &input)
+{
+    return value == input.value;
+}
+
+void Recovered00441880(int *first, int *second)
+{
+    int temporary = *first;
+    *first = *second;
+    *second = temporary;
+}
+
+class Recovered0044C930
+{
+public:
+    int Run(int input);
+};
+
+int Recovered0044C930::Run(int input)
+{
+    return input;
+}
+
+class Recovered00468370
+{
+    char padding[8];
+    int first;
+    int second;
+    int third;
+public:
+    void Clear();
+};
+
+void Recovered00468370::Clear()
+{
+    second = 0;
+    third = 0;
+    first = 0;
+}
+
+class Recovered00476A70
+{
+    int value;
+public:
+    int Equal(const Recovered00476A70 &input);
+};
+
+int Recovered00476A70::Equal(const Recovered00476A70 &input)
+{
+    return value == input.value;
+}
+
+class Recovered004771E0
+{
+public:
+    int Run(int input);
+};
+
+int Recovered004771E0::Run(int input)
+{
+    return input;
+}
+
+void Recovered00478E30(int *first, int *second)
+{
+    int temporary = *first;
+    *first = *second;
+    *second = temporary;
+}
+
+class Recovered0047D7B0
+{
+    int value;
+public:
+    int Equal(const Recovered0047D7B0 &input);
+};
+
+int Recovered0047D7B0::Equal(const Recovered0047D7B0 &input)
+{
+    return value == input.value;
+}
+
+class Recovered0047DC90
+{
+    int value;
+public:
+    int Equal(const Recovered0047DC90 &input);
+};
+
+int Recovered0047DC90::Equal(const Recovered0047DC90 &input)
+{
+    return value == input.value;
+}
+
+void Recovered0047DCC0(int *first, int *second)
+{
+    int temporary = *first;
+    *first = *second;
+    *second = temporary;
+}
+
+void Recovered0047DD00(int *first, int *second)
+{
+    int temporary = *first;
+    *first = *second;
+    *second = temporary;
+}
+
+class Recovered00485C50
+{
+    char padding[12];
+    int first;
+    int second;
+public:
+    int Different();
+};
+
+int Recovered00485C50::Different()
+{
+    return first != second;
+}
+
+class Recovered00486160
+{
+    char padding[8];
+    int first;
+    int second;
+    int third;
+public:
+    void Clear();
+};
+
+void Recovered00486160::Clear()
+{
+    second = 0;
+    third = 0;
+    first = 0;
+}
+
+class Recovered0048BC60
+{
+    char padding[12];
+    int first;
+    int second;
+public:
+    int Different();
+};
+
+int Recovered0048BC60::Different()
+{
+    return first != second;
+}
+
+class Recovered0048BCE0
+{
+    char padding[8];
+    int first;
+    int second;
+    int third;
+public:
+    void Clear();
+};
+
+void Recovered0048BCE0::Clear()
+{
+    second = 0;
+    third = 0;
+    first = 0;
+}
+
+class Recovered0048BD10
+{
+    char padding[12];
+    int first;
+    int second;
+public:
+    int Different();
+};
+
+int Recovered0048BD10::Different()
+{
+    return first != second;
+}
+
+class Recovered0048C290
+{
+    char padding[8];
+    int first;
+    int second;
+    int third;
+public:
+    void Clear();
+};
+
+void Recovered0048C290::Clear()
+{
+    second = 0;
+    third = 0;
+    first = 0;
+}
+
+class Recovered00490F10
+{
+    void Target();
+public:
+    Recovered00490F10 *Run();
+};
+
+Recovered00490F10 *Recovered00490F10::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00490F30
+{
+    void Target();
+public:
+    Recovered00490F30 *Run();
+};
+
+Recovered00490F30 *Recovered00490F30::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00490F50
+{
+    void Target();
+public:
+    Recovered00490F50 *Run();
+};
+
+Recovered00490F50 *Recovered00490F50::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00490F70
+{
+    void Target();
+public:
+    Recovered00490F70 *Run();
+};
+
+Recovered00490F70 *Recovered00490F70::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00490F90
+{
+    void Target();
+public:
+    Recovered00490F90 *Run();
+};
+
+Recovered00490F90 *Recovered00490F90::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00498B60
+{
+    void Target();
+public:
+    Recovered00498B60 *Run();
+};
+
+Recovered00498B60 *Recovered00498B60::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00498B80
+{
+    void Target();
+public:
+    Recovered00498B80 *Run();
+};
+
+Recovered00498B80 *Recovered00498B80::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00498BA0
+{
+    void Target();
+public:
+    Recovered00498BA0 *Run();
+};
+
+Recovered00498BA0 *Recovered00498BA0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00498BC0
+{
+    void Target();
+public:
+    Recovered00498BC0 *Run();
+};
+
+Recovered00498BC0 *Recovered00498BC0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00498BE0
+{
+    void Target();
+public:
+    Recovered00498BE0 *Run();
+};
+
+Recovered00498BE0 *Recovered00498BE0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004A4A20
+{
+    void Target();
+public:
+    Recovered004A4A20 *Run();
+};
+
+Recovered004A4A20 *Recovered004A4A20::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004A6EB0
+{
+    void Target();
+public:
+    Recovered004A6EB0 *Run();
+};
+
+Recovered004A6EB0 *Recovered004A6EB0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004A6ED0
+{
+    void Target();
+public:
+    Recovered004A6ED0 *Run();
+};
+
+Recovered004A6ED0 *Recovered004A6ED0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004A6EF0
+{
+    void Target();
+public:
+    Recovered004A6EF0 *Run();
+};
+
+Recovered004A6EF0 *Recovered004A6EF0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004A6F10
+{
+    void Target();
+public:
+    Recovered004A6F10 *Run();
+};
+
+Recovered004A6F10 *Recovered004A6F10::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004E0670
+{
+    void Target();
+public:
+    Recovered004E0670 *Run();
+};
+
+Recovered004E0670 *Recovered004E0670::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004E0690
+{
+    void Target();
+public:
+    Recovered004E0690 *Run();
+};
+
+Recovered004E0690 *Recovered004E0690::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004EB0E0
+{
+public:
+    unsigned int Get(void *input);
+};
+
+unsigned int Recovered004EB0E0::Get(void *input)
+{
+    return (unsigned int)input >> 4;
+}
+
+class Recovered004EB9F0
+{
+public:
+    unsigned int Get(void *input);
+};
+
+unsigned int Recovered004EB9F0::Get(void *input)
+{
+    return (unsigned int)input >> 4;
+}
+
+class Recovered0054B520
+{
+    void Target();
+public:
+    Recovered0054B520 *Run();
+};
+
+Recovered0054B520 *Recovered0054B520::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered0054B930
+{
+    void Target();
+public:
+    Recovered0054B930 *Run();
+};
+
+Recovered0054B930 *Recovered0054B930::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered0054B950
+{
+    void Target();
+public:
+    Recovered0054B950 *Run();
+};
+
+Recovered0054B950 *Recovered0054B950::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00558080
+{
+    void Target();
+public:
+    Recovered00558080 *Run();
+};
+
+Recovered00558080 *Recovered00558080::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered005610A0
+{
+    void Target();
+public:
+    Recovered005610A0 *Run();
+};
+
+Recovered005610A0 *Recovered005610A0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00563930
+{
+    void Target();
+public:
+    Recovered00563930 *Run();
+};
+
+Recovered00563930 *Recovered00563930::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00567B70
+{
+    void Target();
+public:
+    Recovered00567B70 *Run();
+};
+
+Recovered00567B70 *Recovered00567B70::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00567B90
+{
+    void Target();
+public:
+    Recovered00567B90 *Run();
+};
+
+Recovered00567B90 *Recovered00567B90::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00569580
+{
+    void Target();
+public:
+    Recovered00569580 *Run();
+};
+
+Recovered00569580 *Recovered00569580::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00570440
+{
+    void Target();
+public:
+    Recovered00570440 *Run();
+};
+
+Recovered00570440 *Recovered00570440::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00570460
+{
+    void Target();
+public:
+    Recovered00570460 *Run();
+};
+
+Recovered00570460 *Recovered00570460::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00570480
+{
+    void Target();
+public:
+    Recovered00570480 *Run();
+};
+
+Recovered00570480 *Recovered00570480::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered005704A0
+{
+    void Target();
+public:
+    Recovered005704A0 *Run();
+};
+
+Recovered005704A0 *Recovered005704A0::Run()
+{
+    Target();
+    return this;
+}
---
+++ src/save-server/matches/RecoveredSemanticWrappers.cpp
@@ -0,0 +1,473 @@
+class Recovered0042E010
+{
+    char padding[12];
+    int first;
+    int second;
+public:
+    int Different();
+};
+
+int Recovered0042E010::Different()
+{
+    return first != second;
+}
+
+class Recovered0042E090
+{
+    char padding[8];
+    int first;
+    int second;
+    int third;
+public:
+    void Clear();
+};
+
+void Recovered0042E090::Clear()
+{
+    second = 0;
+    third = 0;
+    first = 0;
+}
+
+class Recovered0042E0C0
+{
+    char padding[12];
+    int first;
+    int second;
+public:
+    int Different();
+};
+
+int Recovered0042E0C0::Different()
+{
+    return first != second;
+}
+
+class Recovered0042E6A0
+{
+    char padding[8];
+    int first;
+    int second;
+    int third;
+public:
+    void Clear();
+};
+
+void Recovered0042E6A0::Clear()
+{
+    second = 0;
+    third = 0;
+    first = 0;
+}
+
+class Recovered0044D540
+{
+    void Target();
+public:
+    Recovered0044D540 *Run();
+};
+
+Recovered0044D540 *Recovered0044D540::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered0044D560
+{
+    void Target();
+public:
+    Recovered0044D560 *Run();
+};
+
+Recovered0044D560 *Recovered0044D560::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered0044D580
+{
+    void Target();
+public:
+    Recovered0044D580 *Run();
+};
+
+Recovered0044D580 *Recovered0044D580::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered0044D5A0
+{
+    void Target();
+public:
+    Recovered0044D5A0 *Run();
+};
+
+Recovered0044D5A0 *Recovered0044D5A0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered0044D5C0
+{
+    void Target();
+public:
+    Recovered0044D5C0 *Run();
+};
+
+Recovered0044D5C0 *Recovered0044D5C0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00455180
+{
+    void Target();
+public:
+    Recovered00455180 *Run();
+};
+
+Recovered00455180 *Recovered00455180::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004551A0
+{
+    void Target();
+public:
+    Recovered004551A0 *Run();
+};
+
+Recovered004551A0 *Recovered004551A0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004551C0
+{
+    void Target();
+public:
+    Recovered004551C0 *Run();
+};
+
+Recovered004551C0 *Recovered004551C0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004551E0
+{
+    void Target();
+public:
+    Recovered004551E0 *Run();
+};
+
+Recovered004551E0 *Recovered004551E0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00455200
+{
+    void Target();
+public:
+    Recovered00455200 *Run();
+};
+
+Recovered00455200 *Recovered00455200::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00461030
+{
+    void Target();
+public:
+    Recovered00461030 *Run();
+};
+
+Recovered00461030 *Recovered00461030::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004634C0
+{
+    void Target();
+public:
+    Recovered004634C0 *Run();
+};
+
+Recovered004634C0 *Recovered004634C0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004634E0
+{
+    void Target();
+public:
+    Recovered004634E0 *Run();
+};
+
+Recovered004634E0 *Recovered004634E0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00463500
+{
+    void Target();
+public:
+    Recovered00463500 *Run();
+};
+
+Recovered00463500 *Recovered00463500::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00463520
+{
+    void Target();
+public:
+    Recovered00463520 *Run();
+};
+
+Recovered00463520 *Recovered00463520::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004B5FF0
+{
+    void Target();
+public:
+    Recovered004B5FF0 *Run();
+};
+
+Recovered004B5FF0 *Recovered004B5FF0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004B6010
+{
+    void Target();
+public:
+    Recovered004B6010 *Run();
+};
+
+Recovered004B6010 *Recovered004B6010::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered004C7620
+{
+public:
+    unsigned int Get(void *input);
+};
+
+unsigned int Recovered004C7620::Get(void *input)
+{
+    return (unsigned int)input >> 4;
+}
+
+class Recovered004C7F30
+{
+public:
+    unsigned int Get(void *input);
+};
+
+unsigned int Recovered004C7F30::Get(void *input)
+{
+    return (unsigned int)input >> 4;
+}
+
+class Recovered00546840
+{
+    void Target();
+public:
+    Recovered00546840 *Run();
+};
+
+Recovered00546840 *Recovered00546840::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00546C50
+{
+    void Target();
+public:
+    Recovered00546C50 *Run();
+};
+
+Recovered00546C50 *Recovered00546C50::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00546C70
+{
+    void Target();
+public:
+    Recovered00546C70 *Run();
+};
+
+Recovered00546C70 *Recovered00546C70::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00556AD0
+{
+    void Target();
+public:
+    Recovered00556AD0 *Run();
+};
+
+Recovered00556AD0 *Recovered00556AD0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00564120
+{
+    void Target();
+public:
+    Recovered00564120 *Run();
+};
+
+Recovered00564120 *Recovered00564120::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00569830
+{
+    void Target();
+public:
+    Recovered00569830 *Run();
+};
+
+Recovered00569830 *Recovered00569830::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered0056DA70
+{
+    void Target();
+public:
+    Recovered0056DA70 *Run();
+};
+
+Recovered0056DA70 *Recovered0056DA70::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered0056DA90
+{
+    void Target();
+public:
+    Recovered0056DA90 *Run();
+};
+
+Recovered0056DA90 *Recovered0056DA90::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00571F30
+{
+    void Target();
+public:
+    Recovered00571F30 *Run();
+};
+
+Recovered00571F30 *Recovered00571F30::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00578DF0
+{
+    void Target();
+public:
+    Recovered00578DF0 *Run();
+};
+
+Recovered00578DF0 *Recovered00578DF0::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00578E10
+{
+    void Target();
+public:
+    Recovered00578E10 *Run();
+};
+
+Recovered00578E10 *Recovered00578E10::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00578E30
+{
+    void Target();
+public:
+    Recovered00578E30 *Run();
+};
+
+Recovered00578E30 *Recovered00578E30::Run()
+{
+    Target();
+    return this;
+}
+
+class Recovered00578E50
+{
+    void Target();
+public:
+    Recovered00578E50 *Run();
+};
+
+Recovered00578E50 *Recovered00578E50::Run()
+{
+    Target();
+    return this;
+}
