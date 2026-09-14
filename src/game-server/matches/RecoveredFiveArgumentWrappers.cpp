5265da5d2450ba81ff39f0f31404c94f965b8889
rbxrootx <rbxrootx@gmail.com>
2026-09-14 13:45:21 -0400

    Match five argument call wrappers

---
+++ src/game-server/matches/RecoveredFiveArgumentWrappers.cpp
@@ -0,0 +1,27 @@
+void TargetRecovered004AA260(int first, int second, int mode, int fourth, int fifth);
+
+void Recovered004AA260(int first, int second)
+{
+    TargetRecovered004AA260(first, second, 1, 0, 0);
+}
+
+void TargetRecovered004AA620(int first, int second, int mode, int fourth, int fifth);
+
+void Recovered004AA620(int first, int second)
+{
+    TargetRecovered004AA620(first, second, 1, 0, 0);
+}
+
+void TargetRecovered004AA6A0(int first, int second, int mode, int fourth, int fifth);
+
+void Recovered004AA6A0(int first, int second)
+{
+    TargetRecovered004AA6A0(first, second, 1, 0, 0);
+}
+
+void TargetRecovered004AAC20(int first, int second, int mode, int fourth, int fifth);
+
+void Recovered004AAC20(int first, int second)
+{
+    TargetRecovered004AAC20(first, second, 1, 0, 0);
+}
---
+++ src/save-server/matches/RecoveredFiveArgumentWrappers.cpp
@@ -0,0 +1,6 @@
+void TargetRecovered004667F0(int first, int second, int mode, int fourth, int fifth);
+
+void Recovered004667F0(int first, int second)
+{
+    TargetRecovered004667F0(first, second, 1, 0, 0);
+}
