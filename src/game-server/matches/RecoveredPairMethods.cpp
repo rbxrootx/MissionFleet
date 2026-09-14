0caf24ab87300083e971985d81f5e5f522bf9807
rbxrootx <rbxrootx@gmail.com>
2026-09-14 11:58:06 -0400

    Match repeated pair operations

---
+++ src/game-server/matches/RecoveredPairMethods.cpp
@@ -0,0 +1,229 @@
+long Recovered004A9F30(long input)
+{
+    return input >= 0 ? input : -input;
+}
+
+long Recovered004B09F0(long input)
+{
+    return input >= 0 ? input : -input;
+}
+
+class Recovered00535640
+{
+    int first;
+    int second;
+public:
+    Recovered00535640(int first_value, int second_value);
+};
+
+Recovered00535640::Recovered00535640(int first_value, int second_value)
+{
+    first = first_value;
+    second = second_value;
+}
+
+class Recovered005356CB
+{
+    int first;
+    int second;
+public:
+    int Compare(int first_value, int second_value);
+};
+
+int Recovered005356CB::Compare(int first_value, int second_value)
+{
+    return first == first_value && second == second_value;
+}
+
+class Recovered00535702
+{
+    int first;
+    int second;
+public:
+    int Compare(int first_value, int second_value);
+};
+
+int Recovered00535702::Compare(int first_value, int second_value)
+{
+    return first != first_value || second != second_value;
+}
+
+class Recovered00535739
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00535739::Apply(int first_value, int second_value)
+{
+    first += first_value;
+    second += second_value;
+}
+
+class Recovered00535762
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00535762::Apply(int first_value, int second_value)
+{
+    first -= first_value;
+    second -= second_value;
+}
+
+class Recovered00535969
+{
+    int first;
+    int second;
+public:
+    Recovered00535969(int first_value, int second_value);
+};
+
+Recovered00535969::Recovered00535969(int first_value, int second_value)
+{
+    first = first_value;
+    second = second_value;
+}
+
+class Recovered005359F4
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered005359F4::Apply(int first_value, int second_value)
+{
+    first += first_value;
+    second += second_value;
+}
+
+class Recovered00535A1D
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00535A1D::Apply(int first_value, int second_value)
+{
+    first += first_value;
+    second += second_value;
+}
+
+class Recovered00535A46
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00535A46::Apply(int first_value, int second_value)
+{
+    first += first_value;
+    second += second_value;
+}
+
+class Recovered00535A6F
+{
+    int first;
+    int second;
+public:
+    int Compare(int first_value, int second_value);
+};
+
+int Recovered00535A6F::Compare(int first_value, int second_value)
+{
+    return first == first_value && second == second_value;
+}
+
+class Recovered00535AA6
+{
+    int first;
+    int second;
+public:
+    int Compare(int first_value, int second_value);
+};
+
+int Recovered00535AA6::Compare(int first_value, int second_value)
+{
+    return first != first_value || second != second_value;
+}
+
+class Recovered00535ADD
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00535ADD::Apply(int first_value, int second_value)
+{
+    first += first_value;
+    second += second_value;
+}
+
+class Recovered00535B06
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00535B06::Apply(int first_value, int second_value)
+{
+    first -= first_value;
+    second -= second_value;
+}
+
+class Recovered00535B2F
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00535B2F::Apply(int first_value, int second_value)
+{
+    first += first_value;
+    second += second_value;
+}
+
+class Recovered00535B58
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00535B58::Apply(int first_value, int second_value)
+{
+    first -= first_value;
+    second -= second_value;
+}
+
+class Recovered00575EB4
+{
+    int first;
+    int second;
+public:
+    Recovered00575EB4(int first_value, int second_value);
+};
+
+Recovered00575EB4::Recovered00575EB4(int first_value, int second_value)
+{
+    first = first_value;
+    second = second_value;
+}
---
+++ src/save-server/matches/RecoveredPairMethods.cpp
@@ -0,0 +1,229 @@
+long Recovered0046A730(long input)
+{
+    return input >= 0 ? input : -input;
+}
+
+long Recovered0046DDA0(long input)
+{
+    return input >= 0 ? input : -input;
+}
+
+class Recovered00531BB0
+{
+    int first;
+    int second;
+public:
+    Recovered00531BB0(int first_value, int second_value);
+};
+
+Recovered00531BB0::Recovered00531BB0(int first_value, int second_value)
+{
+    first = first_value;
+    second = second_value;
+}
+
+class Recovered00531C3B
+{
+    int first;
+    int second;
+public:
+    int Compare(int first_value, int second_value);
+};
+
+int Recovered00531C3B::Compare(int first_value, int second_value)
+{
+    return first == first_value && second == second_value;
+}
+
+class Recovered00531C72
+{
+    int first;
+    int second;
+public:
+    int Compare(int first_value, int second_value);
+};
+
+int Recovered00531C72::Compare(int first_value, int second_value)
+{
+    return first != first_value || second != second_value;
+}
+
+class Recovered00531CA9
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00531CA9::Apply(int first_value, int second_value)
+{
+    first += first_value;
+    second += second_value;
+}
+
+class Recovered00531CD2
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00531CD2::Apply(int first_value, int second_value)
+{
+    first -= first_value;
+    second -= second_value;
+}
+
+class Recovered00531E5C
+{
+    int first;
+    int second;
+public:
+    Recovered00531E5C(int first_value, int second_value);
+};
+
+Recovered00531E5C::Recovered00531E5C(int first_value, int second_value)
+{
+    first = first_value;
+    second = second_value;
+}
+
+class Recovered00531EE7
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00531EE7::Apply(int first_value, int second_value)
+{
+    first += first_value;
+    second += second_value;
+}
+
+class Recovered00531F10
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00531F10::Apply(int first_value, int second_value)
+{
+    first += first_value;
+    second += second_value;
+}
+
+class Recovered00531F39
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00531F39::Apply(int first_value, int second_value)
+{
+    first += first_value;
+    second += second_value;
+}
+
+class Recovered00531F62
+{
+    int first;
+    int second;
+public:
+    int Compare(int first_value, int second_value);
+};
+
+int Recovered00531F62::Compare(int first_value, int second_value)
+{
+    return first == first_value && second == second_value;
+}
+
+class Recovered00531F99
+{
+    int first;
+    int second;
+public:
+    int Compare(int first_value, int second_value);
+};
+
+int Recovered00531F99::Compare(int first_value, int second_value)
+{
+    return first != first_value || second != second_value;
+}
+
+class Recovered00531FD0
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00531FD0::Apply(int first_value, int second_value)
+{
+    first += first_value;
+    second += second_value;
+}
+
+class Recovered00531FF9
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00531FF9::Apply(int first_value, int second_value)
+{
+    first -= first_value;
+    second -= second_value;
+}
+
+class Recovered00532022
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered00532022::Apply(int first_value, int second_value)
+{
+    first += first_value;
+    second += second_value;
+}
+
+class Recovered0053204B
+{
+    int first;
+    int second;
+public:
+    void Apply(int first_value, int second_value);
+};
+
+void Recovered0053204B::Apply(int first_value, int second_value)
+{
+    first -= first_value;
+    second -= second_value;
+}
+
+class Recovered00582594
+{
+    int first;
+    int second;
+public:
+    Recovered00582594(int first_value, int second_value);
+};
+
+Recovered00582594::Recovered00582594(int first_value, int second_value)
+{
+    first = first_value;
+    second = second_value;
+}
