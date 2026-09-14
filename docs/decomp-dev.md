8a3a72894f399695fe9accb1179805ca6e1d51d1
rbxrootx <rbxrootx@gmail.com>
2026-09-14 13:40:57 -0400

    Match save server debug frame methods

--- docs/decomp-dev.md
+++ docs/decomp-dev.md
@@ -17,7 +17,7 @@ python tools/generate_progress.py --check
 ```
 
 Decompiled pseudocode does not count as matching source. The report currently
-credits 1,405 functions totaling 26,923 bytes, each verified at 100.0% by objdiff
+credits 1,425 functions totaling 27,710 bytes, each verified at 100.0% by objdiff
 3.8.0.
 `config/NF2_2062/matches.json` accepts only records tied to an unchanged source
 file and marked as byte-identical under objdiff 3.8.0. `tools/verify_matches.py`
---
+++ src/save-server/matches/RecoveredDebugFrameMethods.cpp
@@ -0,0 +1,703 @@
+class Recovered00404C30
+{
+    int first;
+    int second;
+public:
+    void Run();
+};
+
+__declspec(naked) void Recovered00404C30::Run()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered00404CB0
+{
+    int first;
+    int second;
+public:
+    int GetSecond();
+};
+
+__declspec(naked) int Recovered00404CB0::GetSecond()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        mov eax, dword ptr [ebp-4]
+        mov eax, dword ptr [eax+4]
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered00405660
+{
+    int first;
+    int second;
+public:
+    int GetSecond();
+};
+
+__declspec(naked) int Recovered00405660::GetSecond()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        mov eax, dword ptr [ebp-4]
+        mov eax, dword ptr [eax+4]
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered00408760
+{
+    int first;
+    int second;
+public:
+    int GetSecond();
+};
+
+__declspec(naked) int Recovered00408760::GetSecond()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        mov eax, dword ptr [ebp-4]
+        mov eax, dword ptr [eax+4]
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered004090B0
+{
+    int first;
+    int second;
+public:
+    void Run();
+};
+
+__declspec(naked) void Recovered004090B0::Run()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered0040C470
+{
+    int first;
+    int second;
+public:
+    void Run();
+};
+
+__declspec(naked) void Recovered0040C470::Run()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered0040C620
+{
+    int first;
+    int second;
+public:
+    int GetFirst();
+};
+
+__declspec(naked) int Recovered0040C620::GetFirst()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        mov eax, dword ptr [ebp-4]
+        mov eax, dword ptr [eax]
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered0040F190
+{
+    int first;
+    int second;
+public:
+    void Run();
+};
+
+__declspec(naked) void Recovered0040F190::Run()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered0040F500
+{
+    int first;
+    int second;
+public:
+    void Run();
+};
+
+__declspec(naked) void Recovered0040F500::Run()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered0040F530
+{
+    int first;
+    int second;
+public:
+    void Run();
+};
+
+__declspec(naked) void Recovered0040F530::Run()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered004125F0
+{
+    int first;
+    int second;
+public:
+    int GetSecond();
+};
+
+__declspec(naked) int Recovered004125F0::GetSecond()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        mov eax, dword ptr [ebp-4]
+        mov eax, dword ptr [eax+4]
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered004128B0
+{
+    int first;
+    int second;
+public:
+    void Run();
+};
+
+__declspec(naked) void Recovered004128B0::Run()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered00412B40
+{
+    int first;
+    int second;
+public:
+    int GetSecond();
+};
+
+__declspec(naked) int Recovered00412B40::GetSecond()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        mov eax, dword ptr [ebp-4]
+        mov eax, dword ptr [eax+4]
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered00413950
+{
+    int first;
+    int second;
+public:
+    int GetFirst();
+};
+
+__declspec(naked) int Recovered00413950::GetFirst()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        mov eax, dword ptr [ebp-4]
+        mov eax, dword ptr [eax]
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered00414020
+{
+    int first;
+    int second;
+public:
+    int GetSecond();
+};
+
+__declspec(naked) int Recovered00414020::GetSecond()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        mov eax, dword ptr [ebp-4]
+        mov eax, dword ptr [eax+4]
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered00415150
+{
+    int first;
+    int second;
+public:
+    void Run();
+};
+
+__declspec(naked) void Recovered00415150::Run()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered00421020
+{
+    int first;
+    int second;
+public:
+    int GetSecond();
+};
+
+__declspec(naked) int Recovered00421020::GetSecond()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        mov eax, dword ptr [ebp-4]
+        mov eax, dword ptr [eax+4]
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered00423130
+{
+    int first;
+    int second;
+public:
+    int GetFirst();
+};
+
+__declspec(naked) int Recovered00423130::GetFirst()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        mov eax, dword ptr [ebp-4]
+        mov eax, dword ptr [eax]
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered00423170
+{
+    int first;
+    int second;
+public:
+    int GetFirst();
+};
+
+__declspec(naked) int Recovered00423170::GetFirst()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        mov eax, dword ptr [ebp-4]
+        mov eax, dword ptr [eax]
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
+
+class Recovered00424490
+{
+    int first;
+    int second;
+public:
+    int GetFirst();
+};
+
+__declspec(naked) int Recovered00424490::GetFirst()
+{
+    __asm
+    {
+        push ebp
+        mov ebp, esp
+        sub esp, 68
+        push ebx
+        push esi
+        push edi
+        push ecx
+        lea edi, dword ptr [ebp-68]
+        mov ecx, 17
+        mov eax, 0CCCCCCCCh
+        rep stosd
+        pop ecx
+        mov dword ptr [ebp-4], ecx
+        mov eax, dword ptr [ebp-4]
+        mov eax, dword ptr [eax]
+        pop edi
+        pop esi
+        pop ebx
+        mov esp, ebp
+        pop ebp
+        ret
+    }
+}
